ea490adb9f25b9a2451887f7a043ae37ebbef944
iLaD08 <imadeddinelakhdar08@gmail.com>
2020-12-12 15:13:54 +0100

    added a new project

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -5,11 +5,11 @@ import { ProjectsContainer, ProjectsDiv, ProjectsTitle, ProjectsTitleUnderline }
 const Projects = () => {
     const [data, setData] = React.useState([
         { name: "ilad", description:"My personal website.", link:"https://github.com/iLaD08/ilad" },
+        { name: "Clothing store", description:"A e-commerce website template", link:"https://clothingstore.now.sh/" },
         { name: "todo-app", description:"Todo App template by iLaD", link:"https://github.com/iLaD08/todo-app" },
         { name: "D&L mode", description:"A dark and light mode template.", link:"https://github.com/iLaD08/dark-and-light-mode" },
         { name: "Monster Rolodex", description:"My first React app", link:"https://monsters-rolodex.ilad08.now.sh/" },
-        { name: "Radio bot", description:"A Discord radio bot template.", link:"https://github.com/iLaD08/discord-radio-bot" },
-        { name: "", description:"", link:"" }
+        { name: "Radio bot", description:"A Discord radio bot template.", link:"https://github.com/iLaD08/discord-radio-bot" }
     ]);
 
     return (
