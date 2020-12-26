74ed50895300be5bc7adbf91cce56c234468ebbb
iLaD08 <imadeddinelakhdar08@gmail.com>
2020-12-26 13:55:08 +0100

    go

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -4,14 +4,14 @@ import { ProjectsContainer, ProjectsDiv, ProjectsTitle, ProjectsTitleUnderline }
 
 const Projects = () => {
     const [data, setData] = React.useState([
-        { name: "ilad", description:"My personal website.", link:"https://github.com/iLaD08/ilad" }, 
+        { name: "iLaD", description:"My personal website.", link:"https://github.com/iLaD08/ilad" }, 
         { name: "Clothing store", description:"A e-commerce website template", link:"https://clothingstore.now.sh/" },
         { name: "Todo app", description:"Todo App template by iLaD", link:"https://github.com/iLaD08/todo-app" },
+        { name: "Random Meme", description:"A web app that generate memes from reddit posts.", link:"https://github.com/iLaD08/random-meme" },
         { name: "B3bola chat", description:"A simple real-time-chat app", link:"https://b3bolachat1.glitch.me/" },
         { name: "Monster Rolodex", description:"My first React app", link:"https://monsters-rolodex.ilad08.now.sh/" },
         { name: "Radio bot", description:"A Discord radio bot template.", link:"https://github.com/iLaD08/discord-radio-bot" },
         { name: "D&L mode", description:"A dark and light mode template.", link:"https://github.com/iLaD08/dark-and-light-mode" },
-        { name: "", description:"", link:"" },
         { name: "", description:"", link:"" }
     ]);
 
