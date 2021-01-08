70df6af28a230cb62ed2fa6e8e54e57686b68359
iLaD08 <imadeddinelakhdar08@gmail.com>
2021-01-08 18:58:15 +0100

    added 2 more projects

--- src/pages/project-page/index.js
+++ src/pages/project-page/index.js
@@ -6,12 +6,15 @@ const ProjectPage = () => {
     const [data, setData] = React.useState([
         { name: "iLaD", description:"My personal website.", link:"https://github.com/iLaD08/ilad" }, 
         { name: "Clothing store", description:"A e-commerce website template", link:"https://clothingstore.now.sh/" },
+        { name: "FL-Network", description:"A mern stack project that i made for my dad", link:"https://github.com/FL-Network" },
+        { name: "Protip", description:"A discord bot designed to give helpful advice.", link:"https://github.com/IMAD200" },
         { name: "Todo app", description:"Todo App template by iLaD", link:"https://github.com/iLaD08/todo-app" },
         { name: "Random Meme", description:"A web app that generate memes from reddit posts.", link:"https://github.com/iLaD08/random-meme" },
         { name: "B3bola chat", description:"A simple real-time-chat app", link:"https://b3bolachat1.glitch.me/" },
         { name: "Monster Rolodex", description:"My first React app", link:"https://monsters-rolodex.ilad08.now.sh/" },
         { name: "Radio bot", description:"A Discord radio bot template.", link:"https://github.com/iLaD08/discord-radio-bot" },
         { name: "D&L mode", description:"A dark and light mode template.", link:"https://github.com/iLaD08/dark-and-light-mode" },
+        { name: "", description:"", link:"" },
         { name: "", description:"", link:"" }
     ]);
 
