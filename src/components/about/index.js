58ef9111c43109be03b52d9e7459e834cea69133
iLaD08 <imadeddinelakhdar08@gmail.com>
2021-02-12 17:48:28 +0100

    edited about

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,38 +1,39 @@
 import React from "react";
 import Typewriter from 'typewriter-effect';
-import { AboutContainer,
-         AboutDiv,
-         AboutContentDiv, 
-         AboutTextTyperDiv, 
-         AboutImg, 
-         AboutH2, 
-         AboutP, 
-         AboutButton 
-        }  from "./style";
+import {
+    AboutContainer,
+    AboutDiv,
+    AboutContentDiv,
+    AboutTextTyperDiv,
+    AboutImg,
+    AboutH2,
+    AboutP,
+    AboutButton
+} from "./style";
 
 const About = () => (
     <AboutContainer id="aboutme">
-    <AboutDiv>
-        <AboutContentDiv>
-            <AboutTextTyperDiv>
-             <AboutH2>
-                <Typewriter 
-                    options={{
-                    strings: ['Im ImadEddine Lakhdar'],
-                    autoStart: true,
-                    loop: true 
-                    }}
-                />
-             </AboutH2>   
-            </AboutTextTyperDiv>
-            <AboutP>
-                Also known as iLaD, A 12 years old front-end developer, I love building websites and contribute on open source.
-            </AboutP>
-            <AboutButton onClick={() => window.location.href='/contact'}>Contact me</AboutButton>
-        </AboutContentDiv>
-    </AboutDiv>
-    <AboutImg src="https://i.imgur.com/jvYjBZL_d.webp?maxwidth=728&fidelity=grand" />
-</AboutContainer>
+        <AboutDiv>
+            <AboutContentDiv>
+                <AboutTextTyperDiv>
+                    <AboutH2>
+                        <Typewriter
+                            options={{
+                                strings: ['Im ImadEddine Lakhdar'],
+                                autoStart: true,
+                                loop: true
+                            }}
+                        />
+                    </AboutH2>
+                </AboutTextTyperDiv>
+                <AboutP>
+                    A 12 years old mern stack developer, I love <br/> building websites and contribute on open source.
+                </AboutP>
+                <AboutButton onClick={() => window.location.href = '/contact'}>Contact me</AboutButton>
+            </AboutContentDiv>
+        </AboutDiv>
+        <AboutImg src="https://i.imgur.com/jvYjBZL_d.webp?maxwidth=728&fidelity=grand" />
+    </AboutContainer>
 );
 
 export default About;
