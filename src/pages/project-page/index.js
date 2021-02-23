294b99b37482ba3bfe8588b4d835c4c2864bf7cb
iLaD08 <imadeddinelakhdar08@gmail.com>
2021-02-23 22:15:18 +0100

    updated a project link

--- src/pages/project-page/index.js
+++ src/pages/project-page/index.js
@@ -11,7 +11,7 @@ const ProjectPage = () => {
         { name: "Protip", description: "A discord bot designed to give helpful advice.", link: "https://github.com/IMAD200" },
         { name: "Todo app", description: "Todo App template by iLaD", link: "https://github.com/iLaD08/todo-app" },
         { name: "Random Meme", description: "A web app that generate memes from reddit posts.", link: "https://github.com/iLaD08/random-meme" },
-        { name: "B3bola chat", description: "A simple real-time-chat app", link: "https://b3bolachat1.glitch.me/" },
+        { name: "B3bola chat", description: "A simple real-time-chat app", link: "https://b3bola-chat.herokuapp.com/" },
         { name: "Monster Rolodex", description: "My first React app", link: "https://monsters-rolodex.ilad08.now.sh/" },
         { name: "Radio bot", description: "A Discord radio bot template.", link: "https://github.com/iLaD08/discord-radio-bot" },
         { name: "D&L mode", description: "A dark and light mode template.", link: "https://github.com/iLaD08/dark-and-light-mode" }
