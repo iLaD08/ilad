f622064624c224f91513b706138bd1596a54eea4
iLaD08 <imadeddinelakhdar08@gmail.com>
2021-04-23 18:46:06 +0000

    created NavLogo

--- src/components/responsive-navbar/index.js
+++ src/components/responsive-navbar/index.js
@@ -1,30 +1,26 @@
 import React from "react";
-import { StyledMenu, StyledBurger } from "./style"
+import { StyledMenu, StyledBurger, StyledImg } from "./style";
 
 export const Burger = ({ open, setOpen }) => {
-    return (
-      <StyledBurger open={open} onClick={() => setOpen(!open)}>
-        <div />
-        <div />
-        <div />
-      </StyledBurger>
-    )
+	return (
+		<StyledBurger open={open} onClick={() => setOpen(!open)}>
+			<div />
+			<div />
+			<div />
+		</StyledBurger>
+	);
 };
 
+export const NavLogo = () => <StyledImg src='https://i.imgur.com/jvYjBZL_d.webp?maxwidth=728&fidelity=grand' />;
+
 const ResponsiveNavbar = ({ open }) => {
-    return (
-      <StyledMenu open={open}>
-        <a href="/">
-          Home
-        </a>
-        <a href="/projects">
-          Projects 
-          </a>
-        <a href="/contact">
-          Contact   
-          </a>
-      </StyledMenu>
-    )
-  }
+	return (
+		<StyledMenu open={open}>
+			<a href='/'>Home</a>
+			<a href='/projects'>Projects</a>
+			<a href='/contact'>Contact</a>
+		</StyledMenu>
+	);
+};
 
 export default ResponsiveNavbar;
--- src/components/responsive-navbar/style.js
+++ src/components/responsive-navbar/style.js
@@ -7,7 +7,7 @@ export const StyledMenu = styled.nav`
     flex-direction: column;
     justify-content: center;
     background: #000;
-    transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(-100%)'};
+    transform: ${({ open }) => (open ? "translateX(0)" : "translateX(-100%)")};
     height: 100vh;
     text-align: left;
     padding: 2rem;
@@ -35,46 +35,59 @@ export const StyledMenu = styled.nav`
 `;
 
 export const StyledBurger = styled.button`
-    display: none;
-    @media (max-width: 768px) {
-        position: absolute;
-        top: 5%;
-        left: 2rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-around;
-        width: 2rem;
-        height: 2.5rem;
-        background: transparent;
-        z-index: 1;
-        border: none;
-        cursor: pointer;
-        padding: 4px;
-        z-index: 10;
-        div {
-            width: 2rem;
-            height: 0.25rem;
-            background: ${({ open }) => open ? '#fff' : '#000'};
-            border-radius: 10px;
-            transition: all 0.3s linear;
-            position: relative;
-            transform-origin: 1px;
-        
-            :first-child {
-              transform: ${({ open }) => open ? 'rotate(45deg)' : 'rotate(0)'};
-            }
-        
-            :nth-child(2) {
-              opacity: ${({ open }) => open ? '0' : '1'};
-              transform: ${({ open }) => open ? 'translateX(20px)' : 'translateX(0)'};
-            }
-        
-            :nth-child(3) {
-              transform: ${({ open }) => open ? 'rotate(-45deg)' : 'rotate(0)'};
-            }
-          }
-          &:focus {
-            outline: none;
-          }
-    }
+	display: none;
+	@media (max-width: 768px) {
+		position: absolute;
+		top: 5%;
+		left: 2rem;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-around;
+		width: 2rem;
+		height: 2.5rem;
+		background: transparent;
+		z-index: 1;
+		border: none;
+		cursor: pointer;
+		padding: 4px;
+		z-index: 10;
+		div {
+			width: 2rem;
+			height: 0.25rem;
+			background: ${({ open }) => (open ? "#fff" : "#000")};
+			border-radius: 10px;
+			transition: all 0.3s linear;
+			position: relative;
+			transform-origin: 1px;
+
+			:first-child {
+				transform: ${({ open }) => (open ? "rotate(45deg)" : "rotate(0)")};
+			}
+
+			:nth-child(2) {
+				opacity: ${({ open }) => (open ? "0" : "1")};
+				transform: ${({ open }) =>
+					open ? "translateX(20px)" : "translateX(0)"};
+			}
+
+			:nth-child(3) {
+				transform: ${({ open }) => (open ? "rotate(-45deg)" : "rotate(0)")};
+			}
+		}
+		&:focus {
+			outline: none;
+		}
+	}
+`;
+
+export const StyledImg = styled.img`
+	display: none;
+	@media (max-width: 768px) {
+    display: block;
+    position: fixed;
+    top: 3.5%;
+    right: 3.5%;
+		height: 70px;
+		width: 70px;
+	}
 `;
