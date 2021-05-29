0326350de92c08dbcabdc33ebf31418302c5ad72
iLaD08 <imadeddinelakhdar08@gmail.com>
2021-05-29 16:06:51 +0100

    added some stupid alt/title to skills

--- src/components/skills/index.js
+++ src/components/skills/index.js
@@ -15,11 +15,19 @@ const Skills = () => (
     <SkillsUnderline />
     <SkillsDiv>
       <SkillDiv>
-        <SkillImg src='https://svgur.com/i/QcU.svg' />
+        <SkillImg
+          src='https://svgur.com/i/QcU.svg'
+          alt="I don't have a beard LOL!"
+          title="I don't have a beard LOL!"
+        />
         <Skilltxt>Front-end Developer</Skilltxt>
       </SkillDiv>
       <SkillDiv>
-        <SkillImg src='https://svgur.com/i/Qdu.svg' />
+        <SkillImg
+          src='https://svgur.com/i/Qdu.svg'
+          alt='Umm, yeah cool'
+          title='Umm, yeah cool'
+        />
         <Skilltxt>UI/UX Design</Skilltxt>
       </SkillDiv>
     </SkillsDiv>
