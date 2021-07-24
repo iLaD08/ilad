69d47b7f9448365523ae604e7ba19221032c230f
iLaD08 <imadeddinelakhdar08@gmail.com>
2021-07-24 18:48:01 +0100

    edited responsive-navbar styles

--- src/components/responsive-navbar/style.js
+++ src/components/responsive-navbar/style.js
@@ -7,7 +7,7 @@ export const StyledMenu = styled.nav`
     flex-direction: column;
     justify-content: center;
     background: #000;
-    transform: ${({ open }) => (open ? "translateX(0)" : "translateX(100%)")};
+    transform: ${({ open }) => (open ? "translateX(0)" : "translateX(-100%)")};
     height: 100vh;
     text-align: left;
     padding: 2rem;
@@ -39,7 +39,7 @@ export const StyledBurger = styled.button`
   @media (max-width: 768px) {
     position: absolute;
     top: 5%;
-    right: 2rem;
+    left: 2rem;
     display: flex;
     flex-direction: column;
     justify-content: space-around;
@@ -86,7 +86,7 @@ export const StyledImg = styled.img`
     display: block;
     position: absolute;
     top: 3.5%;
-    left: 3.5%;
+    right: 3.5%;
     height: 70px;
     width: 70px;
   }
